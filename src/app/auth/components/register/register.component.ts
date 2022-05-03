7f36f22b48d700cb5aeccec71499ad3e6731010a
Stanislav <akhmadeevst@gmail.com>
2022-05-03 10:34:39 +0300

    feat: add markup form auth

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: '',
+      email: '',
+      password: '',
+    });
+    console.log(this.form);
+    console.log(this.form.valid);
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
+}
